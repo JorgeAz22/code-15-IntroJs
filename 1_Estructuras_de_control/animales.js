2e4b069ebaabd7abbe9984c9887ecc0953e56577
JorgeAz22 <ascencioluna92@gmail.com>
2022-03-08 21:32:12 -0600

    Clase del dia 08 de marzo

---
+++ 1_Estructuras_de_control/animales.js
@@ -0,0 +1,16 @@
+let animal = prompt("Animal: ")
+
+switch(animal){
+    case "Gato":
+        alert("Miau")
+        break
+    case "Oveja":
+        alert("Meeee")
+        break
+    case "Pato":
+        alert("Cuack cuack")
+        break
+    default:
+        alert("Ni idea como suena")
+        break
+}
